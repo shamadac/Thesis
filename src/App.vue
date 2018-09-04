5c4d239ea493c9fbd2409f4c0a6c24581febd319
Kyle Tozer <kwtozer@gmail.com>
2018-09-03 20:37:16 -0400

    scaffolded vue for front end
    added boostrap
    
    configured dev server

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="text-center">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link class="nav-item nav-link" to="/">Home</router-link>
+            <router-link class="nav-item nav-link" to="/dashboard">Dashboard</router-link>
+            <router-link class="nav-item nav-link" to="/messages">Messages</router-link>
+            <router-link class="nav-item nav-link" to="/notifications">Notifications</router-link>
+          </div>
+        </div>
+      </nav>
+    </header>
+    
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+@import './styles/main.scss';
+</style>
