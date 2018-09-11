ea8a2dc6a23822df87f96d59930163c86a6b4740
Kyle Tozer <kwtozer@gmail.com>
2018-09-11 15:17:17 -0400

    integrating route tests

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
 // sets global variables on process.env from .env file
-require('dotenv').config();
+require('dotenv').config()
 
-var express = require('express');
-var mongoose = require('mongoose');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
-const { NODE_ENV, DB_URL } = process.env;
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const helmet = require('helmet')
+const { seedDatabase } = require('./db/utils')
+const { NODE_ENV, DB_URL } = process.env
 
-const { apiRouter } = require('./routes/index');
+const { apiRouter } = require('./routes/index')
 
-var app = express();
-var db;
+const app = express()
+let db
 
 // connect to database
-mongoose.connect(DB_URL, { useNewUrlParser: true });
-db = mongoose.connection;
+mongoose.connect(DB_URL, { useNewUrlParser: true })
+db = mongoose.connection
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'))
 
 if(NODE_ENV === 'development') {
-  app.use(logger('dev'));
-  db.once('open', () => console.log('Connected to MongoDB'));
+  app.use(logger('dev'))
+  db.once('open', () => console.log('Connected to MongoDB'))
+  seedDatabase()
 }
 
 if(NODE_ENV === 'production') {
   // production middleware here
-  app.use(helmet());
+  app.use(helmet())
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // api route
-app.use('/api', apiRouter);
+app.use('/api', apiRouter)
 
 // error handler
 app.use((err, res, req, next) => {
@@ -46,9 +48,7 @@ app.use((err, res, req, next) => {
 
 // load index.html on all route requests
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
-});
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'))
+})
 
-require('./db/utils').seedDatabase();
-
-module.exports = { app, db };
+module.exports = { app, db }
